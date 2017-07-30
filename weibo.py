91338595dbb16e3167ef1e38cc2359a042908ac7
Zhangv <442478310@qq.com>
2017-07-30 21:57:50 +0800

    weibo delete

---
+++ weibo.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+
+
+browser=webdriver.Firefox(executable_path = 'D:\Python\geckodriver')
+browser.get("http://weibo.com")
+time.sleep(20)
+browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+time.sleep(5)
+browser.find_element_by_xpath("//*[@id=\"v6_pl_rightmod_myinfo\"]/div/div/div[2]/ul/li[3]/a").click()
+time.sleep(5)
+count=0
+
+while True:
+    try:
+        while True:
+            browser.find_element_by_xpath("//*[@id=\"Pl_Official_MyProfileFeed__21\"]//a[@action-type=\"fl_menu\"]").click()
+            browser.find_element_by_xpath("//*[@id=\"Pl_Official_MyProfileFeed__21\"]//a[@action-type=\"feed_list_delete\"]").click()
+            browser.find_element_by_xpath("//*[@id=\"Pl_Official_MyProfileFeed__21\"]//p[@class=\"btn\"]/a").click()
+            browser.refresh()
+            time.sleep(10)
+            count=0
+    except Exception as e:
+        count+=1
+        print("Processing,%d in total"%count)
+        if count>=1000:
+            print("Finished")
+
+
