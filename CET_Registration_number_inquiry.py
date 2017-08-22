4694b31a2f0bd9c28163a9cd9055db6094433108
Zhangv <442478310@qq.com>
2017-08-22 22:19:00 +0800

    finding back your CET registration number if you forgot

---
+++ CET_Registration_number_inquiry.py
@@ -0,0 +1,21 @@
+#帮助忘记CET准考证号的同学找回准考证号
+import json
+import requests
+
+url="http://app.cet.edu.cn:7066/baas/app/setuser.do?method=UserVerify"
+kind=input("查询CET4准考证号请输入1，CET6请输入2:")
+
+ks_data = {
+"ks_xm":input("请输入姓名:"),
+"ks_sfz":input("请输入身份证号："),
+"jb":kind
+}
+
+postdata = {
+"action": "",
+"params":json.dumps(ks_data)
+}
+
+re=requests.post(url,data=postdata)
+
+print(json.loads(re.text)["ks_bh"])
